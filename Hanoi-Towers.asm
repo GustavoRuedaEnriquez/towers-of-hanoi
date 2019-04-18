e37a5422b4fe1f67e96e245a65d528c354e0c43d
Gustavo Rueda <gusgus2208@gmail.com>
2019-04-18 08:32:48 -0500

    Final version

--- Hanoi-Towers.asm
+++ Hanoi-Towers.asm
@@ -9,10 +9,10 @@ main:
     add $t0,$zero,$s0
 
     for:
-        beq $t0,$zero,end_for #¿disks == 0?
-        sw $t0,0($s1)         #Place a disk in Tower A 
-        addi $s1,$s1,4        #Move the Tower A stack pointer one place forward.
-        addi $t0,$t0,-1       #Decrease the next disk's number.
+        beq $t0,$zero,end_for   #¿disks == 0?
+        sw $t0,0($s1)           #Place a disk in Tower A 
+        addi $s1,$s1,4          #Move the Tower A stack pointer one place forward.
+        addi $t0,$t0,-1         #Decrease the next disk's number.
         j for
     end_for: 
      
@@ -33,53 +33,51 @@ main:
 # $a2 - spare - It refers to the auxiliary tower's top.
 # $a3 - finish - It refers to the tower's top where the disk must end.
 hanoiTower:
-	bne $a0, $s4, recursion	#Stop condition: ¿disk == 1?
+    bne $a0, $s4, recursion	#Stop condition: ¿disk == 1?
 	
-	addi $a1,$a1,-4		#(*tower)--
-	lw $t0,0($a1)		#int number = **tower	
-	sw $zero, 0($a1)	#Free the old stack's top		
-	sw $t0,0($a3)		#**tower = disk;		
-	add $a3,$a3,4		#(*tower)++;
-			
-	jr $ra
+    addi $a1,$a1,-4   #(*tower)--
+    lw $t0,0($a1)     #int number = **tower	
+    sw $zero, 0($a1)  #Free the old stack's top		
+    sw $t0,0($a3)     #**tower = disk;		
+    add $a3,$a3,4     #(*tower)++;
+    
+    jr $ra
 	
 recursion:
-	addi $sp,$sp, -8	#Reserve stack.
-	sw $ra,0($sp)		#Store $ra.
-	#sw $a0,4($sp)		#Store disk number.
-	
-	addi $a0,$a0, -1	# disk - 1
-	add $t0,$a2,$zero	#Back up the spare reference.	
-	add $a2,$a3,$zero	#Swap: spare -> finish	
-	add $a3,$t0,$zero	#Swap: finish -> spare	
-	
-	jal hanoiTower
-	
-	add $t0,$a2,$zero	#Back up the finish reference.
-	add $a2,$a3,$zero	#Swap: finish -> spare
-	add $a3,$t0,$zero	#Swap: spare -> finish
-	
-	addi $a1,$a1,-4		#(*tower)--
-	lw $t0,0($a1)		#int number = **tower	
-	sw $zero,0($a1)		#Free the old stack's top		
-	sw $t0,0($a3)		#**tower = disk;		
-	add $a3,$a3,4		#(*tower)++;	
-			
-	add $t0,$a1,$zero	#Back up start reference
-	add $a1,$a2,$zero	#Swap: start -> spare
-	add $a2,$t0,$zero	#Swap: spare -> start
-	
-	jal hanoiTower
-	
-	add $t0,$a1,$zero	#Back up spare reference
-	add $a1,$a2,$zero	#Swap: spare -> start
-	add $a2,$t0,$zero	#Swap: start -> spare
-	
-	addi $a0, $a0, 1
-	
-	lw $ra,0($sp)		#Restore $ra.
-	#lw $a0,4($sp)		#Restore disk last value.
-	addi $sp,$sp,8		#Free stack.		
-	
-	jr $ra
+    addi $sp,$sp, -4    #Reserve stack.
+    sw $ra,0($sp)       #Store $ra.
+    
+    addi $a0,$a0, -1    #disk - 1
+    add $t0,$a2,$zero   #Back up the spare reference.	
+    add $a2,$a3,$zero   #Swap: spare -> finish	
+    add $a3,$t0,$zero   #Swap: finish -> spare	
+    
+    jal hanoiTower
+    
+    add $t0,$a2,$zero   #Back up the finish reference.
+    add $a2,$a3,$zero   #Swap: finish -> spare
+    add $a3,$t0,$zero   #Swap: spare -> finish
+    
+    addi $a1,$a1,-4	    #(*tower)--
+    lw $t0,0($a1)       #int number = **tower	
+    sw $zero,0($a1)     #Free the old stack's top		
+    sw $t0,0($a3)       #**tower = disk;		
+    add $a3,$a3,4       #(*tower)++;	
+    		
+    add $t0,$a1,$zero   #Back up start reference
+    add $a1,$a2,$zero   #Swap: start -> spare
+    add $a2,$t0,$zero   #Swap: spare -> start
+    
+    jal hanoiTower
+    
+    add $t0,$a1,$zero   #Back up spare reference
+    add $a1,$a2,$zero   #Swap: spare -> start
+    add $a2,$t0,$zero   #Swap: start -> spare
+    
+    addi $a0, $a0, 1
+    
+    lw $ra,0($sp)       #Restore $ra.
+    addi $sp,$sp,4      #Free stack.		
+    
+    jr $ra
 exit:
